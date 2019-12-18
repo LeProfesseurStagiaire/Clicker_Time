710382846893318ad877530d3ae810cb1694bcea
LeProfesseurStagiaire <46024910+LeProfesseurStagiaire@users.noreply.github.com>
2019-12-18 17:01:05 +0100

    design improve / collision sand

--- script/main.gd
+++ script/main.gd
@@ -1,21 +1,30 @@
 extends Node2D
 
 var time = 5
-var time_get = 1
+var global_time =0
+var time_get = 0.2
 var sand_count = 0
 var can_sand = true
 onready var sand_ball = preload("res://scene/sand.tscn")
 onready var sand_ball_background = preload("res://scene/sand_background.tscn")
+onready var info_node = preload("res://scene/info.tscn")
 
 func _ready():
 	$time.text = str(int(time))
+	$global_time.text = str(int(global_time))
 
 func _process(delta):
 	time -= 1*delta
+	global_time += 1*delta
 	$time.text = str(int(time))
+	$global_time.text = str(int(global_time))
 
 func _on_TextureButton_pressed():
 	time += time_get
+	var inf = info_node.instance()
+	$info_container.add_child(inf)
+	inf.get_node("Label").text = "+"+str(time_get)
+	inf.position = Vector2(rand_range(5,480),rand_range(5,200))
 	if sand_count < 13 && can_sand:
 		var s = sand_ball.instance()
 		$sand_spawner.add_child(s)
